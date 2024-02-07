d7c9cbef3295dbb72e001afcc806795c481c2978
Andrej Kyselica <andrejky@microsoft.com>
2024-02-07 14:23:32 -0500

    yarp configured with OTel, added back-end project

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Yarp Simple example
 
 This is a simple use of YARP to experiment with instrumentation.
+
+- [X] Get a hello world route running in Yarp
+- [ ] Add OTel config set to send logs, metrics, traces to OTLP endpoing on localhost
+- [ ] Add a second service that YarpProxy will point to, config that with OTel too
--- YarpProxy/Program.cs
+++ YarpProxy/Program.cs
@@ -1,6 +1,36 @@
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// configure OTel and OTLP
+
+const string serviceName = "yarpProxy";
+
+AppContext.SetSwitch("Azure.Experimental.EnableActivitySource", true);
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName))
+        .AddOtlpExporter();
+});
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter())
+    .WithMetrics(metrics => metrics
+        .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter());
+
+
 var app = builder.Build();
 app.MapReverseProxy();
 app.Run();

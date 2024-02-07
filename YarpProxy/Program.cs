6a5f6c12db3fe5b475f41e4a3bf52245f37b3f66
Andrej Kyselica <andrejky@microsoft.com>
2024-02-07 15:11:56 -0500

    minor cleanup

--- YarpProxy/Program.cs
+++ YarpProxy/Program.cs
@@ -8,10 +8,8 @@ builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 // configure OTel and OTLP
-
 const string serviceName = "yarpProxy";
-
-AppContext.SetSwitch("Azure.Experimental.EnableActivitySource", true);
+// AppContext.SetSwitch("Azure.Experimental.EnableActivitySource", true);
 
 builder.Logging.AddOpenTelemetry(options =>
 {
@@ -21,16 +19,21 @@ builder.Logging.AddOpenTelemetry(options =>
                 .AddService(serviceName))
         .AddOtlpExporter();
 });
+
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(serviceName))
     .WithTracing(tracing => tracing
         .AddAspNetCoreInstrumentation()
-        .AddOtlpExporter())
+        // enable YARP distributed tracing
+        // see https://microsoft.github.io/reverse-proxy/articles/distributed-tracing.html
+        .AddSource("Yarp.ReverseProxy") 
+        .AddOtlpExporter()
+    )
     .WithMetrics(metrics => metrics
         .AddAspNetCoreInstrumentation()
         .AddOtlpExporter());
 
-
+// build and start app
 var app = builder.Build();
 app.MapReverseProxy();
 app.Run();
